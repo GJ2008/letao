9d6d04e9d591c1ee8649cdfd186f517aacbe92fc
Daddy <599161490@qq.com>
2018-08-07 16:13:57 +0800

    登录页完善

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -19,6 +19,9 @@ $(function(){
                     max: 6,
                     message: '用户名长度必须在2到6之间'
                     },
+                    callback:{
+                        message: '用户名错误'
+                    }
                 }
             },
             password: {
@@ -33,8 +36,43 @@ $(function(){
                     max: 12,
                     message: '用户名长度必须在6到12之间'
                     },
+                    callback:{
+                        message: '密码错误'
+                    }
                 }
             }
         }
     })
+    $("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑\
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',
+            data:  $("#form").serialize(),
+            dataType: 'json',
+            success: function( info ){
+                // console.log(info);
+                if(info.error === 1000){
+                    $("#form").data('bootstrapValidator').updateStatus('username','INVALID', 'callback')
+                }
+                if(info.error === 1001){
+                    $("#form").data('bootstrapValidator').updateStatus('password','INVALID', 'callback')
+                }
+                if(info.success){
+                    location.href = 'index.html'
+                }
+
+                
+            }
+        })
+    });
+    $('[type="reset"]').click(function(){
+        $("#form").data('bootstrapValidator').resetForm(true);
+    })
+
+
+
+
+
 })
